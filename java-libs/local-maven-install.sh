01d6659d35d3a65b5702d8cec06bdf6f81582e36
ejimenez-ruiz <ejimenez-ruiz@linux-ati0306>
2018-11-01 08:46:19 +0000

    First commit of maven project

---
+++ java-libs/local-maven-install.sh
@@ -0,0 +1,7 @@
+mvn install:install-file -Dfile=align.jar -DgroupId=alignment-api -DartifactId=align -Dversion=4.1 -Dpackaging=jar
+mvn install:install-file -Dfile=ontowrap.jar -DgroupId=alignment-api -DartifactId=ontowrap -Dversion=4.1 -Dpackaging=jar
+mvn install:install-file -Dfile=procalign.jar -DgroupId=alignment-api -DartifactId=procalign -Dversion=4.1 -Dpackaging=jar
+mvn install:install-file -Dfile=DLVwrapper-v4.2.jar -DgroupId=it.unical.dlv -DartifactId=dlv-wrapper -Dversion=4.2 -Dpackaging=jar
+mvn install:install-file -Dfile=elk-0.4.0-owlapi-unofficial.jar -DgroupId=org.semanticweb.elk -DartifactId=elk-owlapi -Dversion=0.4.0.unofficial -Dpackaging=jar
+mvn install:install-file -Dfile=explanation-3.3.0.jar -DgroupId=org.semanticweb.explanation -DartifactId=explanation -Dversion=3.3.0 -Dpackaging=jar
+mvn install:install-file -Dfile=jgraphx_1.10.3.1_modified.jar -DgroupId=com.mxgraph -DartifactId=jgraphx -Dversion=1.10.3.1.modified -Dpackaging=jar
